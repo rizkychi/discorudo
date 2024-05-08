e5a42e612a8e0182eeacb6261a6f8ea013fd6198
Rizky Nur Hidayatullah <rizkynhae@gmail.com>
2024-05-08 12:15:28 +0000

    update

--- discorudo/__init__.py
+++ discorudo/__init__.py
@@ -1,2 +1,3 @@
 from .main import hello
 from .main import hi
+from .main import send_message
--- discorudo/main.py
+++ discorudo/main.py
@@ -3,3 +3,39 @@ def hello():
 
 def hi():
     print('Hi bro! wassup!')
+
+from http.client import HTTPSConnection 
+from sys import stderr 
+from json import dumps 
+import json
+
+# send message to discord channel
+def send_message(conn, channel_id, message, token): 
+    header_data = { 
+        "content-type": "application/json", 
+        "user-agent": "discordapp.com", 
+        "authorization": token
+    }
+    message_data = { 
+		"content": message, 
+		"tts": "false"
+	}
+    ids = ''
+    try: 
+        conn.request("POST", f"/api/v7/channels/{channel_id}/messages", message_data, header_data) 
+        resp = conn.getresponse() 
+         
+        if 199 < resp.status < 300: 
+            print("     - Message Sent!")
+            rs = json.loads(resp.read())
+            ids = rs['id']
+            pass 
+ 
+        else: 
+            stderr.write(f"HTTP {resp.status}: {resp.reason}\n") 
+            pass 
+ 
+    except: 
+        stderr.write("Error\n")
+
+    return ids
